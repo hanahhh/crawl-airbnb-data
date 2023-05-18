ece8da808650192df47c03faf6ab8330909aac61
hanahhh <hoangnhung25122001@gmail.com>
2023-05-18 11:13:12 +0700

    fix csv file

--- crawl/crawl/spiders/airbnb.py
+++ crawl/crawl/spiders/airbnb.py
@@ -2,8 +2,8 @@ import json
 import collections
 from scrapy.spiders import CrawlSpider
 from scrapy_splash import SplashRequest
-import csv
-
+import pandas as pd
+import re
 class Airbnb(CrawlSpider):
 
     name = "airbnb"
@@ -14,7 +14,6 @@ class Airbnb(CrawlSpider):
         self.price_lb = price_lb
         self.price_ub = price_ub
         self.export_data = collections.defaultdict(dict)
-        self.header = {'room_id', 'url', 'avgRatingLocalized', 'city', 'contextualPicturesCount', 'coordinate', 'listingObjType', 'pdpUrlType', 'name', 'roomTypeCategory', 'isSuperHost', 'isVerified', 'reviewCount', 'yearHosting', 'canInstantBook', 'weeklyPriceFactor', 'structuredStayDisplayPrice', 'price', 'maxGuestCapacity'}
 
     def start_requests(self):
         url = f"https://www.airbnb.com.vn/s/{self.city}/homes?locale=vi&query={self.city}&price_min={self.price_lb}&price_max={self.price_ub}&cursor=eyJzZWN0aW9uX29mZnNldCI6MywiaXRlbX"
@@ -40,30 +39,43 @@ class Airbnb(CrawlSpider):
 
                 url = base_url + home.get('listing').get('id')
 
-                data_dict[room_id]['room_id'] = str(room_id)
-                data_dict[room_id]['url'] = str(url)
-                data_dict[room_id]['avgRatingLocalized'] = str(home.get('listing').get('avgRatingLocalized'))
-                data_dict[room_id]['city'] = str(home.get('listing').get('city'))
-                data_dict[room_id]['contextualPicturesCount'] = str(home.get('listing').get('contextualPicturesCount'))
-                data_dict[room_id]['coordinate'] = str(home.get('listing').get('coordinate'))
-                data_dict[room_id]['listingObjType'] = str(home.get('listing').get('listingObjType'))
-                data_dict[room_id]['pdpUrlType'] = str(home.get('pdpUrlType'))
-                data_dict[room_id]['name'] = str(home.get('listing').get('name'))
-                data_dict[room_id]['roomTypeCategory'] = str(home.get('listing').get('roomTypeCategory'))
+                data_dict[room_id]['room_id'] = room_id
+                data_dict[room_id]['url'] = url
+                review_count = ''
+                if home.get('listing').get('avgRatingLocalized'):
+                    match_rating = re.match(r'(\d+,\d+)\s+\((\d+)\)', home.get('listing').get('avgRatingLocalized'))
+                    if match_rating:
+                        data_dict[room_id]['rating'] = match_rating.group(1)
+                        review_count = match_rating.group(2)
+                data_dict[room_id]['city'] = home.get('listing').get('city')
+                data_dict[room_id]['contextualPicturesCount'] = home.get('listing').get('contextualPicturesCount')
+                data_dict[room_id]['lat'] = home.get('listing').get('coordinate').get('latitude')
+                data_dict[room_id]['long'] = home.get('listing').get('coordinate').get('longitude')
+                data_dict[room_id]['listingObjType'] = home.get('listing').get('listingObjType')
+                data_dict[room_id]['pdpUrlType'] = home.get('pdpUrlType')
+                data_dict[room_id]['name'] = home.get('listing').get('name')
+                data_dict[room_id]['roomTypeCategory'] = home.get('listing').get('roomTypeCategory')
+                if len(review_count) > 0:
+                    data_dict[room_id]['reviewCount'] = review_count
                 if primary_host_passport:
-                    data_dict[room_id]['isSuperHost'] = str(primary_host_passport.get('isSuperhost'))
-                    data_dict[room_id]['isVerified'] = str(primary_host_passport.get('isVerified'))
+                    data_dict[room_id]['isSuperHost'] = primary_host_passport.get('isSuperhost')
+                    data_dict[room_id]['isVerified'] = primary_host_passport.get('isVerified')
                     stats = primary_host_passport.get('stats')
 
                     if len(stats) > 0:
-                        data_dict[room_id]['reviewCount'] = str(stats[0].get('value'))
+                        if data_dict[room_id]['reviewCount'] is None:
+                            data_dict[room_id]['reviewCount'] = stats[0].get('value')
                     if len(stats) > 2:
-                        data_dict[room_id]['yearHosting'] = str(stats[2].get('value'))
+                        data_dict[room_id]['yearHosting'] = stats[2].get('value')
 
-                data_dict[room_id]['canInstantBook'] = str(home.get('pricingQuote').get('canInstantBook'))
-                data_dict[room_id]['weeklyPriceFactor'] = str(home.get('pricingQuote').get('weeklyPriceFactor'))
-                data_dict[room_id]['structuredStayDisplayPrice'] = str(home.get('pricingQuote').get('structuredStayDisplayPrice').get('primaryLine').get('qualifier'))
-                data_dict[room_id]['price'] = str(home.get('pricingQuote').get('structuredStayDisplayPrice').get('primaryLine').get('price'))
+                data_dict[room_id]['canInstantBook'] = home.get('pricingQuote').get('canInstantBook')
+                data_dict[room_id]['weeklyPriceFactor'] = home.get('pricingQuote').get('weeklyPriceFactor')
+                data_dict[room_id]['structuredStayDisplayPrice'] = home.get('pricingQuote').get('structuredStayDisplayPrice').get('primaryLine').get('qualifier')
+                if home.get('pricingQuote').get('structuredStayDisplayPrice').get('primaryLine').get('price'):
+                    match_price = re.search(r'\d+(\.\d+)?', home.get('pricingQuote').get('structuredStayDisplayPrice').get('primaryLine').get('price'))
+                    if match_price:
+                        data_dict[room_id]['price'] = match_price.group()
+                        data_dict[room_id]['price_full'] = home.get('pricingQuote').get('structuredStayDisplayPrice').get('primaryLine').get('price')
 
                 self.export_data.update(data_dict)
 
@@ -80,14 +92,9 @@ class Airbnb(CrawlSpider):
                                 args={'wait': '0.5'})
 
     def close(self, spider, reason):
-        with open("out.csv", "w", newline="", encoding='UTF-8') as f:
-            fieldnames = ['1', '2', '5']
-            writer = csv.DictWriter(f, delimiter="\t", fieldnames=self.header)
-            writer.writeheader()
-            for record in self.export_data:
-                print({'SYSTEM_WORKSPACE': 'Value 1', 'price': 1, 'name': "true"}, self.export_data[record])
-
-                writer.writerow(self.export_data[record])
+        df = pd.DataFrame.from_dict(self.export_data, orient='index')
+        csv_file_path = "crawl/output.csv"
+        df.to_csv(csv_file_path, index=False)
 
     def parse_details(self, response, **kwargs):
         id = response.meta['id']
@@ -103,10 +110,9 @@ class Airbnb(CrawlSpider):
             if section['sectionId'] == 'AMENITIES_DEFAULT':
                 list_amenities = data[index]['section']['previewAmenitiesGroups'][0]['amenities']
         if len(self.export_data[id]) > 0:
-            room['maxGuestCapacity'] = str(max_guest_capacity)
+            room['maxGuestCapacity'] = max_guest_capacity
             for amenity in list_amenities:
-                room[amenity['icon']] = str(amenity['available'])
-                self.header.add(amenity['icon'])
+                room[amenity['icon']] = amenity['available']
         self.export_data[id] = room
 
 
